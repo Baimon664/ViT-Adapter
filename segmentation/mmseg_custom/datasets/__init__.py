82518ca7bad21bfba19e2ce3de156b1653cf6f8a
Baimon664 <baimonperasit@gmail.com>
2022-10-26 11:11:51 +0700

    remove register dataset

--- segmentation/mmseg_custom/datasets/__init__.py
+++ segmentation/mmseg_custom/datasets/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .mapillary import MapillaryDataset  # noqa: F401,F403
 from .potsdam import PotsdamDataset  # noqa: F401,F403
-from .bangkokscapes import BangkokScapeDataset
+# from .bangkokscapes import BangkokScapeDataset
 from .pipelines import *  # noqa: F401,F403
